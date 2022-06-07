d46109e482bfb7f89dcf2f9735ebdb9b2b6e3738
Ulises Andres Marin Quinteros <andresquinteros2017rgmail.com>
2022-06-07 13:55:13 -0300

    a3

--- src/components/alert.jsx
+++ src/components/alert.jsx
@@ -2,8 +2,8 @@ import React from 'react';
 const Alert = () => {
 
   return (
-<div class="alert alert-warning alert-dismissible fade show" role="alert">
-  <strong>Hey recuerda!</strong> si estas en smartphone puedes guardar la página como aplicación entrando en: ( ☰ / ⁝ ) y selecciona la opción: (Agregar a la pantalla principal) y listo.
+<div class="alert alert-warning alert-dismissible fade show  rounded-5" role="alert">
+  <strong>Hey remember! </strong>If you are on a smartphone you can save the page as an application by entering: ( <strong>☰</strong> / <strong>⁝</strong> ) and select the option: (Add to the main screen) and that's it.
   <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
 </div>
   );
